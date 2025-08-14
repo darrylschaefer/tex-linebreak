f3ff621eae1df5da88de2f776603cc7cb2c12bff
Darryl Schaefer <119073511+darrylschaefer@users.noreply.github.com>
2025-08-14 09:31:26 -0400

    experimenting with shrink

--- src/html.ts
+++ src/html.ts
@@ -37,7 +37,8 @@ function addItemsForTextNode(
   const el = node.parentNode! as Element;
 
   const spaceWidth = measureFn(el, ' ');
-  const shrink = Math.max(0, spaceWidth - spaceWidth * .0);
+  // const shrink = Math.max(0, spaceWidth - spaceWidth * .0);
+  const shrink = Math.max(0, spaceWidth - 3);
   const hyphenWidth = measureFn(el, '-');
   const isSpace = (word: string) => /\s/.test(word.charAt(0));
 
